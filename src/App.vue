8e16a945a3812a968b68cac525737e719e7c904c
Massimiliano-Novello <massinov5@gmail.com>
2023-04-19 15:22:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=299ac258e58fe881847557d889be5e07&query=").then((resp) => {
+      console.log(resp);
+    })
+  }
+}
+
+</script>
+
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+</style>
