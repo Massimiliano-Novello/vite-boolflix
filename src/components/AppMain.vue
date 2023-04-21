6c3f062b9b4ccb2dab777d5e2f52dc7ac35bbe8d
Massimiliano-Novello <massinov5@gmail.com>
2023-04-21 15:11:15 +0200

    implementazione stelle

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,10 @@
 <script>
 import { store } from '../store';
 import SectionFilm from './SectionFilm.vue';
-import SectionSerie from './SectionSerie.vue';
 export default {
     name: "AppMain",
     components: {
         SectionFilm,
-        SectionSerie
     },
     data () {
         return {
@@ -20,14 +18,14 @@ export default {
     <main class="main">
         <section>
             <h2 class="my-4">Film originali Boolflix</h2>
-            <div class="row">
-                <SectionFilm v-for="(item, index) in store.films" :key="index" :film="item" class="col-1 flex-nowrap"/>
+            <div class="row row-cols-6">
+                <SectionFilm v-for="(item, index) in store.films" :key="index" :film="item" />
             </div>
         </section>
         <section>
             <h2 class="mt-4">Serie tv Netflix</h2>
-            <div class="row row-cols-8">
-                <SectionSerie v-for="(item, index) in store.series" :key="index" :serie="item"/>
+            <div class="row row-cols-6">
+                <SectionFilm v-for="(item, index) in store.series" :key="index" :film="item"/>
             </div>
         </section>
     </main>
@@ -38,5 +36,9 @@ export default {
     background-color: #303030;
     height: calc(100vh - 100px);
     overflow-y: scroll;
+
+    h2 {
+        color: white;
+    }
 }
 </style>
--- src/components/SectionFilm.vue
+++ src/components/SectionFilm.vue
@@ -3,6 +3,14 @@ export default {
     name: "SectionFilm",
     props: {
         film: Object
+    },
+    // data () {
+    //     flags: ["en", "it", "fr"]
+    // },
+    computed: {
+        objVotes() {
+            return Math.ceil(this.film.vote_average / 2)
+        }
     }
 }
 </script>
@@ -13,15 +21,17 @@ export default {
             <img :src="`https://image.tmdb.org/t/p/w342${film.poster_path}`" alt="">
         </div>
         <div class="content-card">
-            <h3>Titolo:{{ film.title }}</h3>
-            <h5>{{ film.original_title }}</h5>
+            <h3>Titolo:<span class="desription">{{ film.title || film.name }}</span></h3>
+            <h5>Titolo originale: <span class="desription">{{ film.original_title || film.original_name }}</span></h5>
             <img v-if="film.original_language === 'en'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1920px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" alt="">
             <img v-else-if="film.original_language === 'it'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Flag_of_Italy_%28Pantone%2C_2003%E2%80%932006%29.svg/220px-Flag_of_Italy_%28Pantone%2C_2003%E2%80%932006%29.svg.png" alt="">
             <img v-else-if="film.original_language === 'fr'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg/280px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg.png" alt="">
             <p v-else > {{ film.original_language }} </p>
             <div class="votes">
-                {{ film.vote_average }}
+                <span class="desription">Voto:</span>
+                <i v-for="num in 5" class="fa-star" :class="num <= objVotes ? 'fa-solid' : 'fa-regular'"></i>              
             </div>
+            <h6>Overview: <span class="description">{{ film.overview }}</span></h6>
         </div>
     </div>
 </template>
@@ -29,43 +39,56 @@ export default {
 <style lang="scss" scoped>
 .card-film {
     height: 100%;
-    position: relative;
-
+    
     .img-card {
         height: 100%;
         img {
+            border: 1px solid white;
             width: 200px;
             height: 300px;
 
 
-        }
-        &:hover {
-            display: none;
         }
     }
         
 
 
     .content-card {
-       display: none;
+        height: 100%;
+    //    display: none;
         h3 {
             font-size: 1.3rem;
+            color: white;
         }
 
         h5 {
             font-size: 1rem;
+            color: white;
         }
 
         p {
             font-size: .8rem;
+            color: white;
+        }
+
+        h6 {
+            font-size: 1rem;
+            color: white;
         }
 
         img {
             width: 20px;
         }
         &:hover .content-card{
-            display: inline-block;
+            display: block;
             background-color: black;
+        }
+
+        .fa-star {
+            color: yellow;
+        }
+        .description {
+            font-size: .7rem;
         }
     }
 
